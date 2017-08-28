f33f3adf443c7c11edcb9b7626d597a9a2853b3d
maxeonyx <maxeonyx@gmail.com>
2017-08-28 18:47:25 +1200

    uninstance gme scene and remove old next_screen logic

--- YOTW.gd
+++ YOTW.gd
@@ -1,8 +1,5 @@
 extends Node
 
-# class member variables go here, for example:
-const game_scene = preload("res://game.tscn")
-
 var explored_scenes = {}
 var x = 0
 var y = 0
@@ -18,21 +15,3 @@ func _lose():
 	
 func _next_screen(dir):
 	print("change")
-	var current_scene = get_node("game")
-	explored_scenes[Vector2(x, y)] = current_scene
-	remove_child(current_scene)
-	
-	if dir == "north":
-		y += 1
-	elif dir == "east":
-		x += 1
-	elif dir == "south":
-		y -= 1
-	elif dir == "west":
-		x -= 1
-	
-	var next_scene = game_scene.instance()
-	next_scene.set_name("game")
-	add_child(next_scene)
-	get_node("game/wagon").connect("lose_signal", self, "_lose")
-	get_node("game/wagon").connect("next_screen_signal", self, "_next_screen")
